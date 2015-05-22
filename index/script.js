b9bc785491cc874fe3eac12613273b8f21924dda
slak44 <sylviu44@gmail.com>
2015-05-22 18:24:02 +0300

    Added initial code input implementation

--- index/script.js
+++ index/script.js
@@ -1,1 +1,7 @@
 'use strict';
+hljs.initHighlighting();
+var codeBlocks = document.getElementsByClassName('code-block');
+for (var i = 0; i < codeBlocks.length; i++) highlightEvt(codeBlocks[i]);
+function highlightEvt(who) {
+  who.addEventListener('blur', function (e) {hljs.highlightBlock(who)});
+}
