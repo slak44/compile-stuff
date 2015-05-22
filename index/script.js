b2672fa229ee0dfaea35a588b654a5d42e858cb7
slak44 <sylviu44@gmail.com>
2015-05-22 19:50:31 +0300

    Implemented client POST sending

--- index/script.js
+++ index/script.js
@@ -3,6 +3,15 @@ hljs.initHighlighting();
 var codeBlocks = document.getElementsByClassName('code-block');
 for (var i = 0; i < codeBlocks.length; i++) highlightEvt(codeBlocks[i]);
 
+function sendToServer() {
+  var data = document.getElementsByClassName('displayable')[0].children[0].innerHTML.replace(/(<\/span>)|(<span(.*?)>)/g, '');
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function () {
+    console.log(this);
+  }
+  xhr.open('POST', document.getElementsByClassName('displayable')[0].classList[0]);
+  xhr.send(data);
+}
 
 function highlightEvt(who) {
   who.addEventListener('blur', function (e) {hljs.highlightBlock(who)});
