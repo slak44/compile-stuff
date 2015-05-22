78eb0398aff011381e46ca3a89a35fa7b6fda1c3
slak44 <sylviu44@gmail.com>
2015-05-22 19:51:03 +0300

    Added POST request processing in server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict';
 const fs = require('fs');
+const entities = new (require('html-entities').AllHtmlEntities)();
 let server;
 if (process.argv[2] === 'use-https') {
   const https = require('https');
@@ -14,12 +15,13 @@ if (process.argv[2] === 'use-https') {
 }
 
 function listener(request, response) {
+  let url = require('url').parse(request.url);
   switch (request.method) {
     case 'GET':
-      processGET(request, response);
+      processGET(request, response, url);
       break;
     case 'POST':
-      sendError(response, 501, 'POST not allowed yet.');
+      processPOST(request, response, url);
       break;
     default: sendError(response, 400, `Bad request: method not supported (${request.method}).`);
   }
@@ -28,8 +30,23 @@ server.listen(8192, function () {
   console.log('Server online.');
 });
 
-function processGET(request, response) {
-  let url = require('url').parse(request.url);
+function processPOST(request, response, url) {
+  let body = '';
+  request.setEncoding('utf8');
+  request.on('data', function (e) {body += e});
+  request.on('end', function () {
+    body = entities.decode(body);
+    switch (url.pathname) {
+      case '/lang-js':
+        sendError(response, 200, 'JS');
+        break;
+      default:
+        
+    }
+  });
+}
+
+function processGET(request, response, url) {
   switch (url.pathname) {
     case '/':
       sendFile(response, 'index/main.html', 'document');
