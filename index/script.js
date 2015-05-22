fc9449b2a7545ae0daaab59a41eda57e0111912d
slak44 <sylviu44@gmail.com>
2015-05-22 18:49:51 +0300

    Code input now works

--- index/script.js
+++ index/script.js
@@ -2,6 +2,13 @@
 hljs.initHighlighting();
 var codeBlocks = document.getElementsByClassName('code-block');
 for (var i = 0; i < codeBlocks.length; i++) highlightEvt(codeBlocks[i]);
+
+
 function highlightEvt(who) {
   who.addEventListener('blur', function (e) {hljs.highlightBlock(who)});
+}
+
+function showLang(langName) {
+  document.getElementsByClassName('displayable')[0].classList.remove('displayable');
+  document.getElementsByClassName(langName)[0].classList.add('displayable');
 }
