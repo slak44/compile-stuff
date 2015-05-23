ce262e318e98449eca67a6089f6f5cf74d954776
slak44 <sylviu44@gmail.com>
2015-05-23 13:59:21 +0300

    Reverted newline removal because of C++ preprocessors

--- index/script.js
+++ index/script.js
@@ -6,7 +6,7 @@ for (var i = 0; i < codeBlocks.length; i++) highlightEvt(codeBlocks[i]);
 
 function sendToServer() {
   var data = document.getElementsByClassName('displayable')[0].children[0].innerHTML // Current language's innerHTML
-    .replace(/<\/span>|<span(.*?)>|<br>|<div>|<\/div>|\n/g, ''); // Remove HTML from syntax highlighting
+    .replace(/<\/span>|<span(.*?)>|<br>|<div>|<\/div>/g, ''); // Remove HTML from syntax highlighting
   var xhr = new XMLHttpRequest();
   xhr.onload = function () {
     console.log(this);
