3c700c5d7eb2ddbf6d9772026fc412810360fe74
Slak44 <sylviu44@gmail.com>
2015-05-27 17:32:05 +0300

    Updated run scripts for linux

--- execute/lang-c++.js
+++ execute/lang-c++.js
@@ -2,7 +2,7 @@
 const cp = require('child_process');
 cp.exec(`g++ ${process.argv[2]} -o ${process.argv[2]}.exe`, function (err, stdout, stderr) {
   if (err) throw err;
-  cp.exec(`${process.argv[2]}.exe`, function (error, stdout, stderr) {
+  cp.exec(`./${process.argv[2]}.exe`, function (error, stdout, stderr) {
     if (error) throw error;
     console.error(stderr);
     console.log(stdout);
